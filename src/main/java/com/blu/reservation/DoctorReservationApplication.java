9df71f45be7fc37d24162f1ea70f8ec62c5538e9
mehraneh <mh.bidkham@gmail.com>
2022-12-30 16:49:26 +0330

    enhancement/like-liquibase-add-users-on-startup

--- src/main/java/com/blu/reservation/DoctorReservationApplication.java
+++ src/main/java/com/blu/reservation/DoctorReservationApplication.java
@@ -1,7 +1,12 @@
 package com.blu.reservation;
 
+import com.blu.reservation.model.PanelUser;
+import com.blu.reservation.model.Role;
+import com.blu.reservation.model.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoctorReservationApplication {
@@ -10,4 +15,11 @@ public class DoctorReservationApplication {
         SpringApplication.run(DoctorReservationApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demoData(UserRepository repo) {
+        return args -> {
+            repo.save(new PanelUser("Doki", "0938188902", Role.DOCTOR));
+            repo.save(new PanelUser("Patient", "09123341231", Role.PATIENT));
+        };
+    }
 }
